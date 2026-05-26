5145a51ac2e1c3514c82844ede6dc5ba23a8ac1b
Mark_TWS <sarmientomark068@gmail.com>
2026-05-26 05:44:19 +0200

    finito pagine e ottimizzazioni

--- Route/JavaScript/Schermata_Add.js
+++ Route/JavaScript/Schermata_Add.js
@@ -142,7 +142,7 @@ items.forEach(item =>{
 
 // INTEGRAZIONE INTELLIGENZA ARTIFICIALE
 
-let TOKEN_HF = "METTER QUI TOKEN RELATIVO A HUGGING FACE";
+// let TOKEN_HF = "METTER QUI TOKEN RELATIVO A HUGGING FACE";
 
 button_Genera.addEventListener("click", async function(){
 
